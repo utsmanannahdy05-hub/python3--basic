867799ceea49a2b7e8df37fbb9cc2cc8bd8b3076
utsman annahdyy <utsmanannahdi@gmail.com>
2025-09-06 20:05:56 +0700

    nambah file ke repo

--- tugas/tugass02.py
+++
@@ -1,6 +0,0 @@
-daftar_rukun_islam = ("syahadat","shalat","zakat","puasa","haji bagi yg mampu")
-print(" rukun islam ada 5 (lima)",daftar_rukun_islam)   
-
-for index in range(len(daftar_rukun_islam)):
-   print(f"{index + 1}. {daftar_rukun_islam[index]}")
-
